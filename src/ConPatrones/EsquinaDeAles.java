a7cf25193d6828749c7976f2b10f30588bcbcbfe
Isabela <ivinces@espol.edu.ec>
2018-07-19 12:48:31 -0500

    ¨articulo¨

--- src/ConPatrones/EsquinaDeAles.java
+++ src/ConPatrones/EsquinaDeAles.java
@@ -7,18 +7,18 @@ public class EsquinaDeAles {
 	}
 	
 	public Articulo fabricarArrozConMenestraYPollo() {
-		Articulo articulo = new Articulo();
-		return articulo;
+		ArticulosFactory articulo = new ArticulosFactory();
+		return articulo.getArticulo("Arroz Con Menestra y Pollo");
 	}
 	
 	public Articulo fabricarPolloAsado() {
-		Articulo articulo = new Articulo();
-		return articulo;
+		ArticulosFactory articulo = new ArticulosFactory();
+		return articulo.getArticulo("Pollo Asado");
 	}
 	
 	public Articulo fabricarArrozConMenestra() {
-		Articulo articulo = new Articulo();
-		return articulo;
+		ArticulosFactory articulo = new ArticulosFactory();
+		return articulo.getArticulo("Arroz con menestra");
 	}
 	
 	//Muchos otros metodos parecidos para otros productos
--- src/ConPatrones/PollosGus.java
+++ src/ConPatrones/PollosGus.java
@@ -7,20 +7,19 @@ public class PollosGus {
 	}
 	
 	public Articulo fabricarArrozConMenestraYPollo() {
-		Articulo articulo = new Articulo(); //Construir 
-		return articulo;
+		ArticulosFactory articulo = new ArticulosFactory();
+		return articulo.getArticulo("Arroz Con Menestra y Pollo");
 	}
 	
 	public Articulo fabricarPolloAsado() {
-		Articulo articulo = new Articulo();
-		return articulo;
+		ArticulosFactory articulo = new ArticulosFactory();
+		return articulo.getArticulo("Pollo Asado");
 	}
 	
 	public Articulo fabricarArrozConMenestra() {
-		Articulo articulo = new Articulo();
-		return articulo;
+		ArticulosFactory articulo = new ArticulosFactory();
+		return articulo.getArticulo("Arroz con menestra");
 	}
-	
 	//Muchos otros metodos parecidos para otros productos
 	
 }
